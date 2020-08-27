bba6e54f58178196b35d693cb0cd2ea8e4cb360d
Kartik Kathuria (33) <Kartik.kathuria_cs19@gla.ac.in>
2020-08-27 20:35:40 +0530

    Add Private Fields of Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -9,5 +9,5 @@ package definitions;
 public class Book {
     private String storeTheNumberOfBooks;
     private String storeNameAuthorBook;
-    private String store13digitISBNumbeBook;
+    private String store13DigitISBNumberBook;
 }
