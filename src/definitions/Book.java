22655c170477c5883f30046b5d9c9c9a654f6edc
Kartik Kathuria (33) <Kartik.kathuria_cs19@gla.ac.in>
2020-08-27 22:02:23 +0530

    Add getters and setters for Private Field of Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -7,7 +7,32 @@
 package definitions;
 
 public class Book {
-    private String storeTheNumberOfBooks;
-    private String storeNameAuthorBook;
-    private String store13DigitISBNumberBook;
+    private String bookName;
+    private String authorName;
+    private String thirteenDigitIsbnNumber;
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getThirteenDigitIsbnNumber() {
+        return thirteenDigitIsbnNumber;
+    }
+
+    public void setThirteenDigitIsbnNumber(String thirteenDigitIsbnNumber) {
+        this.thirteenDigitIsbnNumber = thirteenDigitIsbnNumber;
+    }
 }
+
