c3159149c88e2b7e8996170c1f9ac0b119fb3104
Kartik Kathuria (33) <Kartik.kathuria_cs19@gla.ac.in>
2020-08-27 20:47:35 +0530

    Add Private Fields of Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,10 +1,11 @@
 /*  Created by IntelliJ IDEA.
  *  User: Kartik Kathuria
- *  Date: 21/08/20
+ *  Date: 22/08/20
  *  Time: 3:50 PM
  *  File Name : Library.java
  * */
 package definitions;
 
 public class Library {
+    private Book[] storeAllBooksThatAreCurrentlyAvailable;
 }
